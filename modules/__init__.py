af4c2524a9b27d533ddae1792186e7188c9f683a
DeVenLucaz <ypatole035@gmail.com>
2026-05-30 15:40:46 +0530

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,7 +3,7 @@
 # Version: 0.6.0 | Phase 5 — Polish & Release
 # =============================================================================
 
-VERSION      = "0.7.6"
-VERSION_INFO = (0, 7, 6)
-PHASE        = 6
-PHASE_NAME   = "Library Auto-suggest"
+VERSION      = "0.8.0"
+VERSION_INFO = (0, 8, 0)
+PHASE        = 7
+PHASE_NAME   = "AI FALLBACK"
